cdb903f294b3905a596cb55168c9d52523d2410a
Y4rkiy <70848088+Y4rkiy@users.noreply.github.com>
2022-06-30 19:31:22 +0300

    migrations

---
+++ ASPLab/Migrations/20220630080255_order_info.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASPLab.Migrations
+{
+    public partial class order_info : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
